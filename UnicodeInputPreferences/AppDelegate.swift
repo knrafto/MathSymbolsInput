6ea777b8cf4f9bcf38163b062dad1f5f0ca06b2d
Kyle Raftogianis <knrafto@gmail.com>
2019-12-29 08:16:20 -0800

    Add app to edit preferences

---
+++ UnicodeInputPreferences/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UnicodeInputPreferences
+//
+//  Created by Kyle Raftogianis on 12/29/19.
+//  Copyright © 2019 Kyle Raftogianis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ UnicodeInputPreferences/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UnicodeInputPreferences
+//
+//  Created by Kyle Raftogianis on 12/29/19.
+//  Copyright © 2019 Kyle Raftogianis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
