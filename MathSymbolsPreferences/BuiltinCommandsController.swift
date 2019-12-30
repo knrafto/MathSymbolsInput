ff8dff28a1d7cf8b7e1b20cbae6ef803c26ea2fe
Kyle Raftogianis <knrafto@gmail.com>
2019-12-29 17:40:21 -0800

    Implement filtering built-in commands

--- MathSymbolsPreferences/BuiltinCommandsController.swift
+++ MathSymbolsPreferences/BuiltinCommandsController.swift
@@ -10,8 +10,10 @@ import Foundation
 import Cocoa
 
 class BuiltinCommandsController : NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+  @IBOutlet weak var tableView: NSTableView!
   // Table contents, as a list of (column id -> value) dictionaries.
   var contents: [[String: String]] = []
+  var filteredContents: [[String: String]] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,17 +29,31 @@ class BuiltinCommandsController : NSViewController, NSTableViewDataSource, NSTab
         "replacement": replacement,
       ])
     }
+    filteredContents = contents
   }
 
   // For NSTableViewDataSource.
   func numberOfRows(in tableView: NSTableView) -> Int {
-    return contents.count
+    return filteredContents.count
   }
 
   // For NSTableViewDelegate.
   func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
     let view = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as! NSTableCellView
-    view.textField?.stringValue = contents[row][tableColumn!.identifier.rawValue]!
+    view.textField?.stringValue = filteredContents[row][tableColumn!.identifier.rawValue]!
     return view
+  }
+
+  // Called when the user types in a search.
+  @IBAction func search(_ sender: NSSearchField) {
+    let searchText = sender.stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
+    if searchText.isEmpty {
+      filteredContents = contents
+    } else {
+      filteredContents = contents.filter {
+        return $0["command"]!.contains(searchText) || $0["replacement"]!.contains(searchText)
+      }
+    }
+    tableView.reloadData()
   }
 }
