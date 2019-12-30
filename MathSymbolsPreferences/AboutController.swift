74dbdd0b1b47f2e5a35af00ab3b5c4b09fddcba9
Kyle Raftogianis <knrafto@gmail.com>
2019-12-29 19:34:04 -0800

    Lay out about page

---
+++ MathSymbolsPreferences/AboutController.swift
@@ -0,0 +1,14 @@
+//
+//  AboutController.swift
+//  MathSymbolsPreferences
+//
+//  Created by Kyle Raftogianis on 12/29/19.
+//  Copyright © 2019 Kyle Raftogianis. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class AboutController : NSViewController {
+
+}
