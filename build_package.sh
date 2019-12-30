90f60b1ee035fe5737d4b9a4a16bf176d370797c
Kyle Raftogianis <knrafto@gmail.com>
2019-12-30 12:07:59 -0800

    Fix build script

--- build_package.sh
+++ build_package.sh
@@ -8,6 +8,7 @@ xcodebuild -alltargets -configuration Release
 pkgbuild --install-location "/Library/Input Methods" --component build/Release/MathSymbolsInput.app build/Release/MathSymbolsInput.pkg
 pkgbuild --install-location /Applications --component build/Release/MathSymbolsPreferences.app build/Release/MathSymbolsPreferences.pkg
 
+mkdir -p build/Plugins
 cp -r build/Release/InstallerActivatePane.bundle build/Plugins
 cp InstallerSections.plist build/Plugins
 
