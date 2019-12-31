33d22f4da7951d7b90055c752fa95fd8b1ed9903
Kyle Raftogianis <knrafto@gmail.com>
2019-12-31 15:39:33 -0800

    Fix install_dev_version.sh

--- install_dev_version.sh
+++ install_dev_version.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-sudo cp -r build/Debug/MathSymbolsInput.app "/Library/Input Methods"
-killall MathSymbolsInput
+sudo cp -r "build/Debug/Math Symbols Input.app" "/Library/Input Methods"
+killall -9 "Math Symbols Input"
