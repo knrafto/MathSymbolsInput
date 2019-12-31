d00ea54044bdbf780930a0177a6eeac856a4dfc6
Kyle Raftogianis <knrafto@gmail.com>
2019-12-30 18:25:42 -0800

    Show version and copyright

--- MathSymbolsPreferences/AboutController.swift
+++ MathSymbolsPreferences/AboutController.swift
@@ -10,5 +10,17 @@ import Foundation
 import Cocoa
 
 class AboutController : NSViewController {
+  @IBOutlet weak var versionField: NSTextField!
+  @IBOutlet weak var copyrightField: NSTextField!
 
+  override func viewDidLoad() {
+    let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+    if version != nil {
+      versionField.stringValue = "Version " + version!
+    }
+    let copyright = Bundle.main.object(forInfoDictionaryKey: "NSHumanReadableCopyright") as? String
+    if copyright != nil {
+      copyrightField.stringValue = copyright!
+    }
+  }
 }
