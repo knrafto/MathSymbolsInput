7103898c19a2081eee1582d1fac03dd1082ca455
Kyle Raftogianis <knrafto@gmail.com>
2020-01-01 17:56:04 -0800

    Make installer behavior more sane
    Now it won't:
    * Overwrite your development version instead of installing in the right location
    * Refuse to install if the bundle id changes
    * Refuse to install if you're downgrading versions

--- package.sh
+++ package.sh
@@ -3,11 +3,30 @@ set -eux
 
 xcodebuild -alltargets -configuration Release
 
-pkgbuild --install-location "/Library/Input Methods" --component "build/Release/Math Symbols Input.app" build/Release/MathSymbolsInput.pkg
-pkgbuild --install-location /Applications --component "build/Release/Math Symbols Input - Preferences.app" build/Release/MathSymbolsInputPreferences.pkg
+mkdir -p build/Package
+
+mkdir -p build/Root/MathSymbolsInput
+cp -r "build/Release/Math Symbols Input.app" build/Root/MathSymbolsInput
+ pkgbuild \
+   --root build/Root/MathSymbolsInput \
+   --component-plist MathSymbolsInput-components.plist \
+   --install-location "/Library/Input Methods" \
+   build/Package/MathSymbolsInput.pkg
+
+mkdir -p build/root/MathSymbolsInputPreferences
+cp -r "build/Release/Math Symbols Input - Preferences.app" build/root/MathSymbolsInputPreferences
+ pkgbuild \
+   --root build/root/MathSymbolsInputPreferences \
+   --component-plist MathSymbolsInputPreferences-components.plist \
+   --install-location "/Applications" \
+   build/Package/MathSymbolsInputPreferences.pkg
 
 mkdir -p build/Plugins
 cp -r "build/Release/InstallerActivatePane.bundle" build/Plugins
 cp InstallerSections.plist build/Plugins
 
-productbuild --distribution distribution.xml --package-path build/Release --plugins build/Plugins MathSymbolsInput.pkg
+ productbuild \
+   --distribution distribution.xml \
+   --package-path build/Package \
+   --plugins build/Plugins \
+   MathSymbolsInput.pkg
